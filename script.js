39dcea6682ec9f754756922b47a7c45dff2b7aaa
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-18 23:35:31 +0600

    [ADD]-seoul-city-images-slider

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const images = [
-  "images/pic-1.jpg",
-  "images/pic-2.jpg",
-  "images/pic-3.jpg",
-  "images/pic-4.jpg",
-  "images/pic-5.jpg",
-  "images/pic-6.jpg",
-  "images/pic-7.jpg",
-  "images/pic-8.jpg",
-  "images/pic-9.jpg"
+  "images/imagespic-1.jpg",
+  "images/imagespic-2.jpg",
+  "images/imagespic-3.jpg",
+  "images/imagespic-4.jpg",
+  "images/imagespic-5.jpg",
+  "images/imagespic-6.jpg",
+  "images/imagespic-7.jpg",
+  "images/imagespic-8.jpg",
+  "images/imagespic-9.jpg",
+  "images/imagespic-10.jpg",
+  "images/imagespic-11.jpg",
+  "images/imagespic-12.jpg",
+  "images/imagespic-13.jpg",
+  "images/imagespic-14.jpg",
+  "images/imagespic-15.jpg",
 ];
 
 
@@ -20,4 +26,4 @@ setInterval(() => {
   const imgUrl = images[imgIndex];
   imgEl.setAttribute("src", imgUrl);
   imgIndex++;
-}, 500);
+}, 1500);
