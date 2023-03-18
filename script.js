4f1cec585a17560f12fa7bdfdee67285f057f35d
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-18 23:02:52 +0600

    [ADD]-image-slider-with-setInterval

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+const images = [
+  "images/pic-1.jpg",
+  "images/pic-2.jpg",
+  "images/pic-3.jpg",
+  "images/pic-4.jpg",
+  "images/pic-5.jpg",
+  "images/pic-6.jpg",
+  "images/pic-7.jpg",
+  "images/pic-8.jpg",
+  "images/pic-9.jpg"
+];
 
 
 let imgIndex = 0;
+const imgEl = document.getElementById("slider-img");
 setInterval(() => {
-  console.log(imgIndex);
+  if (imgIndex === images.length) {
+    imgIndex = 0;
+  }
+  const imgUrl = images[imgIndex];
+  imgEl.setAttribute("src", imgUrl);
   imgIndex++;
-}, 1000);
+}, 500);
